9749c59b10555f46daa73ae789c850cf3f795704
MehdiYsl <mehdi.yansli@gmail.com>
2023-03-27 14:39:22 +0200

    mise en place du footer et navbar dans la base et d'une carte pour la page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\JobRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+
+    public function index(JobRepository $jobRepository, Request $request): Response
     {
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
